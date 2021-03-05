2f0df19ae286d24ad26d1412e189c0b75cc85672
Ryan Young <ryany@spotify.com>
2021-03-04 22:22:08 -0500

    ecto for form validation

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule Qwixx.MixProject do
       {:phoenix, "~> 1.5.8"},
       {:phoenix_live_view, "~> 0.15.1"},
       {:floki, ">= 0.27.0", only: :test},
+      {:ecto, "~> 3.5"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.4"},
