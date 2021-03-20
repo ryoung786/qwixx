69c38936beda99cf1905c10c15acd97f8932c3b7
Ryan Young <ryoung786@gmail.com>
2021-03-20 02:59:17 -0400

    prod host url

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :qwixx, QwixxWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "qwixx.ryoung.info", port: 4001],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["https://qwixx.ryoung.info"]
 
 # Do not print debug messages in production
 config :logger, level: :info
