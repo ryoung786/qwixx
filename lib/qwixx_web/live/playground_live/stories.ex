1ccbd05d63628adfafe112606b9c6484e49e827f
Ryan Young <ryoung786@gmail.com>
2025-02-09 15:45:01 -0500

    playground text color defaults; delete usage block

--- lib/qwixx_web/live/playground_live/stories.ex
+++ lib/qwixx_web/live/playground_live/stories.ex
@@ -9,17 +9,22 @@ defmodule QwixxWeb.PlaygroundLive.Stories do
 
   embed_templates "*.html"
 
+  attr :id, :string, required: true
+  slot :description
+  slot :title
+  slot :usage
+  slot :inner_block
+
   def story(assigns) do
     ~H"""
-    <div id={@id} class="mb-8">
+    <div id={@id} class="mb-8 text-slate-100">
       <h1 class="text-2xl font-bold mb-2  text-yellow-500 drop-shadow-md">{render_slot(@title)}</h1>
-      <p class="text-slate-400 mb-4">{render_slot(@description)}</p>
+      <p class="mb-4">{render_slot(@description)}</p>
 
       <Card.card class="w-full p-4 mb-4">
         {render_slot(@inner_block)}
       </Card.card>
 
-      <h2 class="text-lg font-semibold mb-2">Usage</h2>
       {render_slot(@usage)}
     </div>
     """
